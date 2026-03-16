4839262cc3b582712cd08ad48bd9eca40cdc4b7f
shailendra849 <shailendrad699@gmail.com>
2026-03-17 00:29:15 +0530

    Update UrlShortenerApplicationTests.java

--- src/test/java/com/thenriquedb/url_shortener/UrlShortenerApplicationTests.java
+++ src/test/java/com/thenriquedb/url_shortener/UrlShortenerApplicationTests.java
@@ -1,4 +1,4 @@
-package com.thenriquedb.url_shortener;
+package com.shailendra.url_shortener-service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
